60a0a2b55d41ea6bf6cb77d82d27584c80dd5034
Jacob Meneses <jaykobyou@gmail.com>
2024-05-23 18:07:12 -0600

    New endpoint for moving task to another column

--- src/controllers/task.ts
+++ src/controllers/task.ts
@@ -6,10 +6,50 @@ export const router = Router();
 router.get('/', async (req: Request, res) => {
   const tasks: object[] | null = await prisma.task.findMany({
   });
-	const columns: object[] | null = await prisma.column.findMany({});
+  const columns: object[] | null = await prisma.column.findMany({});
 
   res.send({
-		columns,
-		tasks,
-	});
+    columns,
+    tasks,
+  });
 });
+
+interface MoveTaskRequest extends Request {
+  body: {
+    columnId: number;
+    taskId: number;
+  }
+};
+
+router.put('/move', async (req: MoveTaskRequest, res) => {
+  const taskId : number = req.body.taskId;
+  const columnId : number = req.body.columnId;
+
+  const task : object | null = await prisma.task.findUnique({
+    where: {
+      id: taskId,
+    }
+  });
+
+  if ( !task ) {
+    res.status(404).send({ message: 'Task not found' });
+    return;
+  }
+
+  try {
+    const updatedTask = await prisma.task.update({
+      where: {
+        id: taskId,
+      },
+      data: {
+        columnId
+      }
+    });
+  } catch (error) {
+    res.status(500).send({ message: 'Error updating task' });
+    return;
+  }
+
+  res.send({ message: 'Task updated' });
+});
+
